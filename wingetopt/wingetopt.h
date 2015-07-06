a128a0b0ab2772de5c8761b69e0bb31da4bf686d
Lucas Kahlert <lucas.kahlert@tu-dresden.de>
2015-07-06 11:08:40 +0200

    Next draft for getopt port on Visual Studio [ci skip]

--- wingetopt/wingetopt.h
+++ wingetopt/wingetopt.h
@@ -20,11 +20,26 @@
 extern "C" {
 #endif
 
-extern int opterr;
-extern int optind;
-extern int optopt;
-extern char *optarg;
-extern int getopt(int argc, char **argv, char *opts);
+extern int opterr;        /* if error message should be printed */
+extern int optind;        /* index into parent argv vector */
+extern int optopt;        /* character checked for validity */
+extern int optreset;        /* reset getopt */
+extern char *optarg;        /* argument associated with option */
+
+struct option
+{
+  const char *name;
+  int has_arg;
+  int *flag;
+  int val;
+};
+
+#define no_argument       0
+#define required_argument 1
+#define optional_argument 2
+
+int getopt(int, char**, char*);
+int getopt_long(int, char**, char*, struct option*, int*);
 
 #ifdef __cplusplus
 }
